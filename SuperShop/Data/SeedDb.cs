d0865370b99bdc7c21e5f64333e42dd89245b0c4
FranciscoRosa9 <frnuno9@gmail.com>
2021-06-27 18:34:09 +0100

    SeedDb

---
+++ SuperShop/Data/SeedDb.cs
@@ -0,0 +1,49 @@
+﻿using SuperShop.Data.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        private Random _random;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+            _random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            if (!_context.Products.Any())
+            {
+                await Task.Run(() => 
+                {
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        AddProduct($"Product {i}");
+                    }
+                });
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string productName)
+        {
+            _context.Products.Add(new Product
+            {
+                Name = productName,
+                Price = _random.Next(5, 1001),
+                IsAvailable = true,
+                Stock = _random.Next(10, 101)
+            });
+        }
+    }
+}
--- SuperShop/Program.cs
+++ SuperShop/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SuperShop.Data;
 
 namespace SuperShop
 {
@@ -7,7 +9,21 @@ namespace SuperShop
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            RunSeeding(host);
+            host.Run();
+        }
+
+        private static void RunSeeding(IHost host)
+        {
+            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var seeder = scope.ServiceProvider.GetService<SeedDb>();
+
+                seeder.SeedAsync().Wait();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
